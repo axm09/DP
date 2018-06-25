d236781fb88e4b950fcf976b7e41fa56a9164bd7
kmalhi <karan.malhi@gmail.com>
2018-06-24 18:35:52 -0700

    A change to see if maven is triggered

--- src/main/java/axm09/App.java
+++ src/main/java/axm09/App.java
@@ -1,5 +1,7 @@
 package axm09;
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        String abbreviated = StringUtils.abbreviate("United nations organization", 3);
+        System.out.println(abbreviated);
+        System.out.println("Some code change");
     }
 }
