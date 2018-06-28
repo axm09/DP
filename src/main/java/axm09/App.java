e1dbe69530a8615a142d92c310dc8b9cf590d7e4
axm09 <40548928+axm09@users.noreply.github.com>
2018-06-28 01:46:34 -0700

    Update App.java

--- src/main/java/axm09/App.java
+++ src/main/java/axm09/App.java
@@ -11,7 +11,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        String abbreviated = StringUtils.abbreviate("United nations organization", 3);
+        String abbreviated = StringUtils.abbreviate("United nations organization", 16);
         System.out.println(abbreviated)
         
     }
